ee8d72f5330067a5a162e6748b984a6328091887
AnushrutiBansiwalRA2411003011776 <ab6941@srmist.edu.in>
2026-04-01 15:04:44 +0530

    Base application setup

---
+++ src/BookMyStay.java
@@ -0,0 +1,11 @@
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("=== Book My Stay App ===");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Welcome to the application!");
+
+    }
+}
